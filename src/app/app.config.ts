65e5f42dcca34e26086179dbbc8111c78a61c82e
gustavo hernandez <hernandez29a@gmail.com>
2026-06-27 21:05:17 -0400

    Agregar configuración de proveedores para LucideAngularModule en app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { LucideAngularModule, Globe, Search, User, Twitter, Facebook, Linkedin } from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom(LucideAngularModule.pick({ Globe, Search, User, Twitter, Facebook, Linkedin }))
+  ]
 };
